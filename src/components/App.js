f074e91e2ebbfb9efd432a4b30da573c5d1833a5
coderchaubey <sunnychaubeysm@gmail.com>
2023-06-14 22:49:46 +0530

    most outdated test case

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
 
 import React from "react";
 import './../styles/App.css';
-import Parent from "./parent";
+import parent from "./parent";
 
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
-        <Parent/>
+        <parent/>
     </div>
   )
 }
